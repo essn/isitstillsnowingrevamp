98a5ff8fc4f43b60f2ac52a7098af73aa46dbc80
Jesse Pledger <jpledger@datalot.com>
2022-07-06 17:39:48 -0400

    Initial commit

---
+++ lib/isitstillsnowing_revamp.ex
@@ -0,0 +1,18 @@
+defmodule IsitstillsnowingRevamp do
+  @moduledoc """
+  Documentation for `IsitstillsnowingRevamp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IsitstillsnowingRevamp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/isitstillsnowing_revamp_test.exs
@@ -0,0 +1,8 @@
+defmodule IsitstillsnowingRevampTest do
+  use ExUnit.Case
+  doctest IsitstillsnowingRevamp
+
+  test "greets the world" do
+    assert IsitstillsnowingRevamp.hello() == :world
+  end
+end
