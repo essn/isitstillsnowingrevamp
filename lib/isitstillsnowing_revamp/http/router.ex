77025ed133d600ae236e3d1f3fa25478ab506be5
Jesse Pledger <jessepledger@gmail.com>
2022-07-09 15:42:29 -0400

    Super basic render html file

--- lib/isitstillsnowing_revamp/http/router.ex
+++ lib/isitstillsnowing_revamp/http/router.ex
@@ -2,14 +2,15 @@ defmodule IsitstillsnowingRevamp.Http.Router do
   use Plug.Router
 
   plug(Plug.Logger, log: :debug)
-  plug(Plug.CSRFProtection)
 
   plug(:match)
 
   plug(:dispatch)
 
   get "/" do
-      send_resp(conn, 200, "world")
+    IO.inspect(conn)
+    |> put_resp_header("content-type", "text/html; charset=utf-8")
+    |> send_file(200, "priv/assets/index.html")
   end
 
   get _ do
