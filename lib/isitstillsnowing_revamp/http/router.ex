5f00fae99f98c86ba117627db824a5b3361a6859
Jesse Pledger <jessepledger@gmail.com>
2022-07-09 18:26:36 -0400

    Render layout and cacluate weather - wip

--- lib/isitstillsnowing_revamp/http/router.ex
+++ lib/isitstillsnowing_revamp/http/router.ex
@@ -1,6 +1,8 @@
 defmodule IsitstillsnowingRevamp.Http.Router do
   use Plug.Router
 
+  import IsitstillsnowingRevamp.{Renderer.Layout, Weather}
+
   plug(Plug.Logger, log: :debug)
 
   plug(:match)
@@ -8,12 +10,20 @@ defmodule IsitstillsnowingRevamp.Http.Router do
   plug(:dispatch)
 
   get "/" do
-    IO.inspect(conn)
+    %{remote_ip: ip} = conn
+
+    format_ip(ip) |> IO.inspect()
+
+    build_ip_info("62.182.98.212") |> IO.inspect()
+
+    conn
     |> put_resp_header("content-type", "text/html; charset=utf-8")
-    |> send_file(200, "priv/assets/index.html")
+    |> send_resp(200, layout())
   end
 
   get _ do
     send_resp(conn, 404, "not found")
   end
+
+  defp format_ip(ip), do: Tuple.to_list(ip) |> Enum.join(".")
 end
