02e4e5d3bd5b7c56bd6dc31ce74730a2a09036d8
Jesse Pledger <jessepledger@gmail.com>
2022-07-09 19:57:26 -0400

    Remove boilerplate

--- lib/isitstillsnowing_revamp.ex
+++ lib/isitstillsnowing_revamp.ex
@@ -1,18 +1,3 @@
 defmodule IsitstillsnowingRevamp do
-  @moduledoc """
-  Documentation for `IsitstillsnowingRevamp`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> IsitstillsnowingRevamp.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  @moduledoc false
 end
